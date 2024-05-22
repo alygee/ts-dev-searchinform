4e24ee6582b7667da7cb1e4d11a3301f5635c589
Albert Allagulov <aallagulov@satorilab.ru>
2024-05-22 11:56:28 +0300

    create user list item component

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,13 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import UserListItem from './UserListItem.vue'
+
+const users = ref([
+  { avatar: '../assets/images/user-1.jpg', name: 'Alice' },
+  { avatar: '../assets/images/user-2.jpg', name: 'Julia' }
+])
+</script>
+
 <template>
   <div class="w-1/4 bg-white border-r border-gray-300">
     <div class="overflow-y-auto h-screen p-3 mb-9 pb-20">
-      <div class="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
-        <div class="w-12 h-12 bg-gray-300 rounded-full mr-3">
-          <img src="@/assets/images/user-1.jpg" alt="User Avatar" class="w-12 h-12 rounded-full" />
-        </div>
-        <div class="flex-1">
-          <h2 class="text-lg font-semibold">Alice</h2>
-        </div>
+      <div v-for="user in users">
+        <UserListItem :avatar="user.avatar" :name="user.name" />
       </div>
     </div>
   </div>
---
+++ src/components/UserListItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps(['name', 'avatar'])
+
+function getImageUrl(name) {
+  return new URL(name, import.meta.url).href
+}
+</script>
+
+<template>
+  <div class="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
+    <div class="w-12 h-12 bg-gray-300 rounded-full mr-3">
+      <img :src="getImageUrl(avatar)" :alt="name" class="w-12 h-12 rounded-full" />
+    </div>
+    <div class="flex-1">
+      <h2 class="text-lg font-semibold">{{ name }}</h2>
+    </div>
+  </div>
+</template>
