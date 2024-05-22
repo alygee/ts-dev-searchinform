847baf3dff1b1fa98c2a34139c7c650524399c5d
Albert Allagulov <aallagulov@satorilab.ru>
2024-05-22 11:39:12 +0300

    create user list component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
+<script setup lang="ts">
+import UserList from './components/UserList.vue'
+</script>
+
 <template>
   <div class="max-w-screen-lg mx-auto bg-blue-100 h-screen">
-    <div class="w-1/4 bg-white border-r border-gray-300">
-      <div class="overflow-y-auto h-screen p-3 mb-9 pb-20">
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </div>
+    <UserList />
   </div>
 </template>
---
+++ src/components/UserList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-1/4 bg-white border-r border-gray-300">
+    <div class="overflow-y-auto h-screen p-3 mb-9 pb-20">
+      <div class="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
+        <div class="w-12 h-12 bg-gray-300 rounded-full mr-3">
+          <img src="@/assets/images/user-1.jpg" alt="User Avatar" class="w-12 h-12 rounded-full" />
+        </div>
+        <div class="flex-1">
+          <h2 class="text-lg font-semibold">Alice</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
